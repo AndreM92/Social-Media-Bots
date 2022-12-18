f1af7ec1b18e4d740023fd74870b7f7350d28feb
André Muth <andre.muth@tutanota.com>
2022-12-18 17:44:35 +0100

    Update InstagramBot.py

--- InstagramBot.py
+++ InstagramBot.py
@@ -16,6 +16,23 @@ import pandas as pd
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Driverbug function
+def fixDriverbug():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    driver.get('https://www.instagram.com/accounts/login/')
+    time.sleep(1)
+    if loginpage != driver.current_url:
+        print('There is something wrong with the page')
+
+# Pyautogui Investigation process
+# time.sleep(3)
+# x,y = pyautogui.position()
+# print(str(x)+ ','+ str(y))
+# 930,777
+
 ##############################################################################
 # Login process
 
@@ -109,35 +126,57 @@ except:
         time.sleep(1)
         pyautogui.moveTo(942,775)
         pyautogui.click() 
-
+        
+startpage = driver.current_url
+        
 ###############################################################################    
-# Searching Process
-
+# Searching Process on Instagram
 # Activate searchbutton
 target = 'libertarian_monk'
-try:
-    WebDriverWait(driver,1).until(EC.presence_of_element_located((By.XPATH,'/html/body')))
-    driver.find_element('xpath','/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/a/div/div[1]/div/div/svg').click()
-except NoSuchElementException:
-    time.sleep(3)
-    pyautogui.moveTo(21,379)
-    pyautogui.click()
-time.sleep(1)    
 
-# Search for a profile
-searchbutton = driver.find_element('xpath','/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[1]/div/input')
-searchbutton.send_keys(target)
-time.sleep(1)
-try:
-    driver.find_element('xpath','//*[@id="mount_0_0_+3"]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div/a/div/div[1]/div/span/img').click()
-except NoSuchElementException:
+def getsearchbox():
+    try:
+        WebDriverWait(driver,1).until(EC.presence_of_element_located((By.XPATH,'/html/body')))
+        driver.find_element('xpath','/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/a/div/div[1]/div/div/svg').click()
+    except NoSuchElementException:
+        try:
+            driver.find_element(By.CSS_SELECTOR,"[aria-label='Suche']").click()
+        except NoSuchElementException:
+            try:
+                time.sleep(1)
+                pyautogui.moveTo(21,379)
+                pyautogui.click()
+                time.sleep(1)
+            except Exception as e:
+                print(repr(e))       
+    try:
+        searchbutton = driver.find_element('xpath','/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div[1]/div/input')
+    except:
+        try:
+            searchbox = driver.find_element(By.CSS_SELECTOR,"[aria-label='Sucheingabe']")
+            return searchbox
+        except:
+            return ''
+
+def getprofile(target):
+    searchbox = getsearchbox()
+    searchbox.send_keys(target)
     time.sleep(1)
-    pyautogui.moveTo(149,384)
-    pyautogui.click()
-time.sleep(2)
+#        searchbox.send_keys(Keys.RETURN)
+    searchbox.send_keys(Keys.ENTER)
+    try:
+        searchbox.send_keys(Keys.RETURN)
+    except:
+        pass
+    time.sleep(1)
+    if target not in driver.current_url:        
+        pyautogui.moveTo(149,384)
+        pyautogui.click()  
+                    
+getprofile(target)  
 
 ##############################################################################
-# Scraping the profile stats
+# Scrape the profile stats
 class ScrapedProfile:
     def __init__(self):
         # to prevent errors:
@@ -162,3 +201,59 @@ dfProfiles = pd.DataFrame(columns = ['name','url','follower','following','postin
 profile = ScrapedProfile()
 dfProfiles.loc[len(dfProfiles)] = [profile.name,profile.url,profile.follower,profile.following,profile.postings,profile.desc,profile.link]
 print(dfProfiles)
+
+##############################################################################
+# Crawl through the page and postings
+# First we have to scroll down to load the postings up to a specific date
+# Additionally I also limited the function to 10 scrolls
+month = 'Dezember'
+year = 2018
+
+def crawler(month,year):
+    count =  0
+    links = []
+    scrheight = driver.execute_script('return document.body.scrollHeight')
+    while True:
+        driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+        time.sleep(3)
+        newheight = driver.execute_script('return document.body.scrollHeight')
+        pyautogui.moveTo(1625,910)
+        pyautogui.click()
+        time.sleep(2)
+        # The xpath of the postings changes after scrolling down, so we need to catch the links before they aren't visible anymore
+        soup = BeautifulSoup(driver.page_source,'lxml')
+        linkpart = [l['href'] for l in soup.find_all('a',href=True) if l['href'][:3] == '/p/']
+        plinks = ['https://www.instagram.com/' + l for l in linkpart]
+        links = links + plinks
+        try:
+            date = driver.find_element(By.CLASS_NAME,'_aaqe').text.lower()
+        except:
+            date = driver.find_element(By.CLASS_NAME,'_aacl').text.lower()
+        # This will probably also work with "[aria-label='close']"
+        driver.find_element(By.CSS_SELECTOR,"[aria-label='Schließen']").click()
+        time.sleep(1)
+        
+        if month.lower() in date and str(year) in date:
+            # Return the links and remove duplicates of the collected links without losing the order
+            return list(dict.fromkeys(links))
+            break
+        count += 1
+        if count == 4:
+            return list(dict.fromkeys(links))
+            break
+       
+plinks = (crawler(month,year))
+print(len(plinks))
+
+
+# Bonus: 
+# Follow
+driver.find_element('xpath','//*[@id="mount_0_0_aw"]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/div[1]/div[1]/div/div/button/div/div').click()
+# Like
+driver.find_element(By.CSS_SELECTOR,"[aria-label='Gefällt mir']").click()
+# Commnent
+slot = driver.find_element(By.CSS_SELECTOR,"[aria-label^='Kommentieren']")
+slot.click()
+slot.send_keys("That's amazing!")
+time.sleep(1)
+slot.send_keys(Keys.ENTER)
